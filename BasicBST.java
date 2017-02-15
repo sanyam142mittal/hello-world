3b92ff48c634d0a1b925c038a5757bc2b45c1285
sanyam142mittal <sanyam142mittal@gmail.com>
2017-02-15 10:18:57 +0530

    Update BasicBST.java

--- BasicBST.java
+++ BasicBST.java
@@ -54,6 +54,25 @@ public class BST
     
   }
   
+  public Node FindLCA(Node root, int smallValue, int largeValue)
+  {
+   	if(root!=null)
+    {
+     	if(root.key> largeValue)
+        {
+         	return FindLCA(root.left,smallValue,largeValue); 
+        }
+      	else if(root.key < smallValue)
+        {
+         	return FindLCA(root.right,smallValue,largeValue); 
+        }
+      	return root;
+      
+    }
+    
+    return null;
+  }
+  
 }
 
 public class executor
@@ -62,11 +81,21 @@ public class executor
   public static void main(String[] args)
   {
     BST mytree = new BST();
-    mytree.Insert(50);
-    mytree.Insert(10);
-    mytree.Insert(60);
+    mytree.Insert(18);
+    mytree.Insert(9);
+    mytree.Insert(36);
+    mytree.Insert(6);
     mytree.Insert(12);
+    mytree.Insert(10);
+    mytree.Insert(1);
+    mytree.Insert(8);
     //System.out.println("10");
-    mytree.Inorder(mytree.root);
+    //mytree.Inorder(mytree.root);
+    
+    System.out.println(mytree.FindLCA(mytree.root,1,10).key);
+    
+    System.out.println(mytree.FindLCA(mytree.root,1,36).key);
+    
+    System.out.println(mytree.FindLCA(mytree.root,1,8).key);
   }
 }
